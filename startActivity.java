43e1f5dc2ba77349710440397ad917582c286152
LevinithD <39787294+LevinithD@users.noreply.github.com>
2019-11-09 20:48:15 +0100

    Update startActivity.java

--- startActivity.java
+++ startActivity.java
@@ -31,36 +31,6 @@ public class startActivity extends AppCompatActivity {
             case R.id.MCSA70740:
                 quizCategory = 4;
                 break;
-            case R.id.MCSA70483:
-                quizCategory = 5;
-                break;
-            case R.id.MCSA70486:
-                quizCategory = 6;
-                break;
-            case R.id.MCSA70480:
-                quizCategory = 7;
-                break;
-            case R.id.MCSA70741:
-                quizCategory = 8;
-                break;
-            case R.id.MCSA70742:
-                quizCategory = 9;
-                break;
-            case R.id.MD100:
-                quizCategory = 10;
-                break;
-            case R.id.ITILFoundationv4:
-                quizCategory = 11;
-                break;
-            case R.id.PRINCE2Foundation:
-                quizCategory = 12;
-                break;
-            case R.id.MCSA70778:
-                quizCategory = 13;
-                break;
-            case R.id.MCSA70779:
-                quizCategory = 14;
-                break;
         }
 
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
