1cc1432146d0bcc054b4f6b19472d0fd3f7437b5
LevinithD <39787294+LevinithD@users.noreply.github.com>
2020-06-09 21:03:26 +0200

    Update startActivity.java

--- startActivity.java
+++ startActivity.java
@@ -6,37 +6,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class startActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
+
+     TextView txt_pathShow;
+     Button btn_filePicker;
+     Intent myFileIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        txt_pathShow = (TextView)findViewById(R.id.txt_path);
+        btn_filePicker = (Button) findViewById(R.id.btn_filePicker);
+
+        btn_filePicker.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+                    public void onClick(View view)
+                    {
+                       myFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                       myFileIntent.setType("*/*");
+                       startActivityForResult(myFileIntent,10);
+
+                    }
+        });
     }
 
-    public void startQuiz(View view) {
+     @Override
+     protected void onActivityResult( int requestCode, int resultCode, @Nullable Intent data) 
+     {
+         super.onActivityResult(requestCode, resultCode, data);
+            
+         switch (requestCode)
+            { 
+                case 10:
+                    if(resultCode==RESULT_OK)
+                    {
+                        String path = data.getData().getPath();
+                        txt_pathShow.setText(path);
 
-        int quizCategory = 0; // All
+                    }
+                break;
+            }
 
-        switch (view.getId()) {
-            case R.id.ITILv3:
-                quizCategory = 1;
-                break;
-            case R.id.AZ900:
-                quizCategory = 2;
-                break;
-            case R.id.MS100:
-                quizCategory = 3;
-                break;
-            case R.id.MCSA70740:
-                quizCategory = 4;
-                break;
-        }
-
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        intent.putExtra("QUIZ_CATEGORY", quizCategory);
-        startActivity(intent);
     }
-}
-
-
